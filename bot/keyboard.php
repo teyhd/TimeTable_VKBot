6fde978aea406281a2d6d14499c67e95771a7ba8
teyhd <45236519+teyhd@users.noreply.github.com>
2019-10-07 18:19:08 +0400

    ADD_tomorrow

--- bot/keyboard.php
+++ bot/keyboard.php
@@ -7,6 +7,9 @@ function keybrd($param){
         case 2:
          $keybrd = '{"one_time":true,"buttons":[[{"action":{"type":"text","payload":"{\"button\": \"1\"}","label":"Расписание"},"color":"positive"},{"action":{"type":"text","payload":"{\"button\": \"2\"}","label":"Знакомство"},"color":"primary"}]]}';
          break; 
+         case 3:
+         $keybrd = '{"one_time":false,"buttons":[[{"action":{"type":"text","payload":"{\"button\": \"1\"}","label":"Да"},"color":"positive"},{"action":{"type":"text","payload":"{\"button\": \"2\"}","label":"Нет"},"color":"negative"}]]}';
+         break; 
          default:
               $keybrd = '{"one_time":true,"buttons":[[{"action":{"type":"text","payload":"{\"button\": \"1\"}","label":"Расписание"},"color":"positive"}]]}';
          break;

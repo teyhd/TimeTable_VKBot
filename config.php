34ee9f4f2f5aa6cb237411ab5162d641a7566202
teyhd <45236519+teyhd@users.noreply.github.com>
2019-10-07 12:27:07 +0400

    predict func

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
 define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
 define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
 
-define('CALLBACK_API_CONFIRMATION_TOKEN', '7bf03bf8'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('CALLBACK_API_CONFIRMATION_TOKEN', '96c1ea68'); //Строка для подтверждения адреса сервера из настроек Callback API
 define('VK_API_ACCESS_TOKEN', '22370387787625350d0dd61b51199ec54727b577ddb20a2a1082713659b3de10278d2a98467d497e0154d'); //Ключ доступа сообщества
 define('YANDEX_API_KEY', '36ba9849-6c36-4427-9cf9-7dcb6d380a91'); //Ключ для доступа к Yandex Speech Kit
